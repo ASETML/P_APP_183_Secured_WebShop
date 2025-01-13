14d857bffadbaa351d8ac5d4bcc4501c728cc99b
ASETML <alban.segalen@eduvaud.ch>
2025-01-13 15:31:41 +0100

    Ajouter(Script de création): Ajout du script de création de la DB et MCD

---
+++ DB/CreateDB.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE db_webShop;
+USE db_webShop;
+
+CREATE TABLE t_users(
+   users_id INT AUTO_INCREMENT,
+   username VARCHAR(50) NOT NULL,
+   password VARCHAR(256) NOT NULL,
+   PRIMARY KEY(users_id),
+   UNIQUE(username)
+);
