ddeadc353f5c025cad6decf68fc2ef16ac73d66d
ASETML <alban.segalen@eduvaud.ch>
2025-01-24 12:05:04 +0100

    Créer(vues): Utilisation de EJS pour le frontend

--- Application/app/routes/User.js
+++ Application/app/routes/User.js
@@ -1,6 +1,44 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/UserController");
-router.get('/', controller.get) 
-module.exports = router; 
+router.get("/", controller.get);
+
+router.get("/login", (req, res) => {
+  res.render("login", { accountName: "Alban" });
+});
+
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.redirect("/user");
+  //TODO déplacer dans /user
+  res.render("user", { accountName: req.body.username });
+});
+
+router.get("/register", (req, res) => {
+  res.render("register", { accountName: "Alban" });
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  // Get the client
+  const mysql = require("mysql2");
+
+  // Create the connection to database
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: "6033",
+    user: "root",
+    password: "root",
+    database: "mysql",
+  });
+
+  // Using placeholders
+  connection.query("SELECT * FROM users", function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  });
+
+  res.render("user", { accountName: req.body.username });
+});
+
+module.exports = router;
--- Application/app/server.js
+++ Application/app/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 
-
 const app = express();
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
 
+app.use(express.json());
+//Middleware qui rempli req.body
+app.use(express.urlencoded());
 
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+const userRoute = require("./routes/User");
+
+app.use("/user", userRoute);
 
 // Démarrage du serveur
 app.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log("Server running on port 8080");
 });
