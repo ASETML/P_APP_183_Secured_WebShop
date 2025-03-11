8bc7693fdab9191b87a377e3d8150b604da4cc81
ASETML <alban.segalen@eduvaud.ch>
2025-03-11 12:52:22 +0100

    refactor(UserController.get): suppression de la requête en base
    Comme toutes les infos sont dans le token, on n'a pas besoin de la requête

--- Application/app/controllers/UserController.js
+++ Application/app/controllers/UserController.js
@@ -3,40 +3,27 @@ const jsonwebtoken = require("jsonwebtoken");
 
 module.exports = {
   get: (req, res) => {
-    db.connect();
-    db.query(
-      "SELECT username FROM t_users WHERE users_id = ?",
-      [req.params.id],
-      function (error, results, fields) {
-        //res.render("admin", { accountName: "Admin", userList: results });
-        if (error) throw error;
-        if (results.length > 0) {
-          const token = req.cookies.token;
-          try {
-            const decodedToken = jsonwebtoken.verify(
-              token,
-              process.env.SECRETKEY
-            );
-            if (decodedToken.id == req.params.id) {
-              res.render("user", {
-                accountName: results[0].username,
-                id: req.params.id,
-              });
-            } else {
-              res.render("user", {
-                accountName: "Forbidden",
-                id: "403",
-              });
-            }
-          } catch (error) {
-            console.log(error);
-            //return res.status(401).json({ error: "Le token n'est pas valide" });
-            return res.render("login", { accountName: "" });
-          }
-        } else {
-          res.status(401).json({ error: "Invalid username or password" });
-        }
+    const token = req.cookies.token;
+    try {
+      //On vérifie le token
+      const decodedToken = jsonwebtoken.verify(token, process.env.SECRETKEY);
+      //L'id de l'utilisateur est le même que la page à laquelle il veut accéder
+      if (decodedToken.id == req.params.id) {
+        //On affiche la page
+        res.render("user", {
+          accountName: decodedToken.username,
+          id: decodedToken.id,
+        });
+      } else {
+        //Il n'a pas le droit
+        res.render("user", {
+          accountName: "Forbidden",
+          id: "403",
+        });
       }
-    );
+    } catch (error) {
+      //On revient sur la page de login en cas de problème
+      return res.render("login", { accountName: "" });
+    }
   },
 };
